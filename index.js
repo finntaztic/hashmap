7dc7aca11503865b9b448f16dd312dca4b3110e2
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-08-21 08:57:52 +0800

    Save:

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 
 
-class HashMap(){
-
-  let loadFactor = 0;
-  let capacity = 0;
+class HashMap {
+  loadFactor; 
+  capacity;
 
   hash(key){
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++){
       console.log(`initial hash: ${hashCode}`)
-      hashCode = primeNumber * hashCode + key.charCodeAt(i) % 16;
+      hashCode = primeNumber * (hashCode % 16) + key.charCodeAt(i);
+      console.log(`second hash: ${hashCode}`)
       console.log(`prime: ${primeNumber}`)
       console.log(`hash: ${hashCode}`)
       console.log(`key: ${key.charCodeAt(i)}`)
     }
-    return hashCode //% 16;
+    return (`my hashcode: ${hashCode}`) //% 16;
   }
 
+  set (key, value){
+    
+  }
 }
 
-
+const hasher = new HashMap();
+console.log(hasher.hash('josephine'));
+// console.log(hasher.hash('k'));
 
 
   //  if (index < 0 || index >= buckets.length) {
